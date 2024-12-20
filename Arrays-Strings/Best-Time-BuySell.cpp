3251a4144c2819400c29527ac10d2cc56263e009
Boris <borisnikolic2302@gmail.com>
2024-12-20 16:28:36 +0100

    Best Time to buy and sell

--- Arrays-Strings/Best-Time-BuySell.cpp
+++ Arrays-Strings/Best-Time-BuySell.cpp
@@ -1,7 +1,35 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
 #include <vector>
-class Solution {
-  int maxProfit(std::vector<int> &prices) {}
 
+class Solution {
 public:
+  int maxProfit(std::vector<int> &prices) {
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for (int price : prices) {
+      if (price < minPrice) {
+        minPrice = price;
+      } else {
+        int profit = price - minPrice;
+        maxProfit = std::max(maxProfit, profit);
+      }
+    }
+
+    return maxProfit;
+  }
 };
-int main() { return 0; }
+
+int main() {
+  Solution solution;
+
+  std::vector<int> prices = {7, 1, 5, 3, 6, 4};
+  int profit = solution.maxProfit(prices);
+
+  std::cout << "Maximum profit: " << profit << std::endl;
+
+  return 0;
+}
+
